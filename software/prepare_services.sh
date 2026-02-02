790d8d46ea92d6c600d32097441f1c49a857b53f
Salvatore Dello Iacono <delloiaconos@gmail.com>
2026-02-02 14:44:25 +0100

    Renamed BME software into bme-grabber

--- software/prepare_services.sh
+++ software/prepare_services.sh
@@ -10,23 +10,21 @@ if [ -n "${VIRTUAL_ENV:-}" ]; then
   deactivate
 fi
 
-
-
 # Create the target installation directory under /opt.
-mkdir -p "$DEST_DIR"
+sudo mkdir -p "$DEST_DIR"
+sudo chown "$(id -un):$(id -gn)" "$DEST_DIR"
 
 # Copy all helper scripts/resources from the local ./scripts directory
 # into the installation directory.
 cp -r $BASE_DIR/scripts/* "$DEST_DIR/"
 
 # Build the project from source and copy the binary to destination.
-cd $BASE_DIR/bme688-linux
+cd "$BASE_DIR/bme688-linux"
 make clean
 make all
-cp ./out/bme-logger "$DEST_DIR/"
+cp "$BASE_DIR/bme688-linux/out/bme-grabber" "$DEST_DIR/"
 # Ensure executable permission.
-chmod +x "$DEST_DIR/bme-logger"
-
+chmod +x "$DEST_DIR/bme-grabber"
 
 # Move into the installation directory
 cd "$DEST_DIR"
--- software/scripts/bme-publisher.sh
+++ software/scripts/bme-publisher.sh
@@ -3,4 +3,4 @@ set -euo pipefail
 
 BME_BASE="/opt/bme"
 
-exec stdbuf -oL $BME_BASE/bme-logger | $BME_BASE/env/bin/python $BME_BASE/stream_publisher.py --quiet --topic-prefix "measures/$(hostname)"
+exec stdbuf -oL $BME_BASE/bme-grabber | $BME_BASE/env/bin/python $BME_BASE/stream_publisher.py --quiet --topic-prefix "measures/$(hostname)"
